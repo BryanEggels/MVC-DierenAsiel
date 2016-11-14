dfe1385cf0824e4693e9c901fc04c1c416eea8d3
Bryan Eggels <356103@student.fontys.nl>
2016-11-14 16:06:12 +0100

    first commit

---
+++ MVC Test/Controllers/DierController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Test.Controllers
+{
+    public class DierController : Controller
+    {
+        // GET: Dier
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
